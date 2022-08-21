29ae05f9515fcae23641b1b8f09576f3c1cdbfdf
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-08-21 20:47:26 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -3,5 +3,13 @@ a = 11;
 let b;
 b = a;
 let c = 9;
+
 var myFirstName = "ilia";  
 var myLastName = "kravtsov";
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
