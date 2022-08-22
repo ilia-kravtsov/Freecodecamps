c0b7d52a30600ce9a55ac40f30ffbc2c5e374b62
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-08-22 20:42:24 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -35,3 +35,12 @@ console.log(FCC, fact); // Change this line
 const sum = 10 + 10;
 
 const difference = 45 - 33;
+
+const product = 8 * 0;
+
+const quotient = 66 / 33;
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
