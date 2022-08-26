174439b07bb0092eff7e2a318f9adfab0998bacc
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-08-26 20:31:28 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -17,6 +17,16 @@ var a = 9;
 var myFirstName = "ilia";
 var myLastName = "kravtsov";
 
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
 var a;
 a = 11;
 let b;
