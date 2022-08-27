804be8b6ece32757a540c76bd600fc90b87fec0c
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-08-27 19:52:40 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -212,3 +212,8 @@ const myArray = [["liga", 11], ["laizz", 7]];
 
 const myArray = [50, 60, 70];
 const myData = myArray[0];
+
+// Setup
+const myArray = [18, 64, 99];
+myArray[0] = 45;
+// Only change code below this line
