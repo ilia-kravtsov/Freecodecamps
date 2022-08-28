f8938ae57024da215cee644c4434e886de548057
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-08-28 20:26:07 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -271,3 +271,35 @@ function timesFive(a) {
 }
 
 const answer = (timesFive(5));
+
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+function myLocalScope() {
+  // Only change code below this line
+  const myVar = "a";
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
