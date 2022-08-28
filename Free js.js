d673eb4bbcda40a01086cf2e6dc48fbbb3c3dbfa
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-08-28 18:06:21 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -244,3 +244,10 @@ const myArray = [["John", 23], ["dog", 3]];
 
 // Only change code below this line
 const removedFromMyArray = myArray.shift(0);
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
