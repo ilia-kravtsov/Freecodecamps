52d0958fecd2f8fcf22a9dc7cf8471ecc148066a
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-08-28 21:23:10 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -315,3 +315,20 @@ function myOutfit() {
 }
 
 myOutfit();
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
