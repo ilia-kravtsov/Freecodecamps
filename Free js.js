e91c4e7b233a81d11b92599058ce25d45fcf7916
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-09-18 20:38:43 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -448,3 +448,16 @@ function testLessOrEqual(val) {
 }
 
 testLessOrEqual(10);
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
