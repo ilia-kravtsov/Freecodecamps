8b202193090396b07a35ff4f3b627351e265bc0c
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-09-18 19:45:57 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -352,3 +352,13 @@ function testEqual(val) {
 }
 
 testEqual(10);
+
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
