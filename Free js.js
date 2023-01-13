02f6c2edc9dcad28d9aab0ea0d4164d8694a3ba6
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-01-13 15:15:46 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -2,41 +2,6 @@
 /* this is a 
 multi-line comment */
 
-var myName;
-
-var a;
-a = 7;
-
-var a;
-a = 7;
-var b;
-b = a;
-
-var a = 9;
-
-var myFirstName = "ilia";
-var myLastName = "kravtsov";
-
-// Only change code below this line
-var a = 5;
-var b = 10;
-var c = "I am a";
-// Only change code above this line
-
-a = a + 1;
-b = b + 5;
-c = c + " String!";
-
-// Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
-
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
-
 var a;
 a = 11;
 let b;
@@ -71,15 +36,15 @@ let fact = "is cool!"; // Change this line
 fact = "is awesome!";
 console.log(FCC, fact); // Change this line
 
-const sum = 10 + 10;
+const summ = 10 + 10;
 
 const difference = 45 - 33;
 
-const product = 8 * 0;
+const productt = 8 * 0;
 
-const quotient = 66 / 33;
+const quotientt = 66 / 33;
 
-let myVar = 87;
+let myVarr = 87;
 
 // Only change code below this line
 myVar++;
@@ -110,8 +75,8 @@ b += 9;
 c += 7;
 
 let a = 11;
-let b = 9;
-let c = 3;
+let bb = 9;
+let cc = 3;
 
 // Only change code below this line
 a -= 6;
@@ -119,8 +84,8 @@ b -= 15;
 c -= 1;
 
 let a = 5;
-let b = 12;
-let c = 4.6;
+let bbb = 12;
+let ccc = 4.6;
 
 // Only change code below this line
 a *= 5;
@@ -128,28 +93,28 @@ b *= 3;
 c *= 10;
 
 let a = 48;
-let b = 108;
-let c = 33;
+let bv = 108;
+let cv = 33;
 
 // Only change code below this line
 a /= 12;
 b /= 4;
 c /= 11;
 
-const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+const myStrr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 
-const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+ myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
-const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+ myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 
-const myStr = "This is the start. " + "This is the end."; // Change this line
+ myStr = "This is the start. " + "This is the end."; // Change this line
 
-let myStr = "This is the first sentence. ";
-myStr += "This is the second sentence.";
+ myStr = "This is the first sentence. ";
+ myStr += "This is the second sentence.";
 
 // Only change code below this line
 const myName = "ilia";
-const myStr = "My name is " + myName + "and I am well!";
+ myStr = "My name is " + myName + "and I am well!";
 
 // Change code below this line
 const someAdjective = "pain";
@@ -775,3 +740,32 @@ const myDog = {
 
 // Only change code below this line
 myDog.name = "Happy Coder";
+
+/* 
+                                              Selecting from Many Options with Switch Statements
+
+                                              
+*/
+
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
