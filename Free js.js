df698b18c81ccbd16d2b6a31d10dc710d8004c55
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-02-16 12:40:57 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -3192,41 +3192,104 @@ console.log(typeofseven + three);
 console.log(typeof seven)
 console.log(typeof three)
 
-// __________________________________________________ 
+// __________________________________________________ Catch Misspelled Variable and Function Names
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
+// __________________________________________________ Catch Unclosed Parentheses, Brackets, Braces and Quotes
+
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
+
+// __________________________________________________ Catch Mixed Usage of Single and Double Quotes
+
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
+
+// __________________________________________________ Catch Use of Assignment Operator Instead of Equality Operator
+
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x === y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
+
+// __________________________________________________ Catch Missing Open and Closing Parenthesis After a Function Call
+
+function getNine() {
+  let x = 6;
+  let y = 3;
+  return x + y;
+}
+
+let result = getNine();
+console.log(result);
+
+// __________________________________________________ Catch Arguments Passed in the Wrong Order When Calling a Function
+
+function raiseToPower(b, e) {
+  return b ** e
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
+
+// __________________________________________________ Catch Off By One Errors When Using Indexing
+
+function countToFive() {
+  let firstFive = "12345";
+  let len = firstFive.length;
+  // Only change code below this line
+  for (let i = 0; i <= len-1; i++) {
+  // Only change code above this line
+    console.log(firstFive[i]);
+  }
+}
+
+countToFive();
+
+// __________________________________________________ Use Caution When Reinitializing Variables Inside a Loop
+
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+    row = []
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      row.push(0);
+    }
+    // Pushes the current row, which now has n zeroes in it, to the array
+    newArray.push(row);
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
 
 
+// __________________________________________________ Prevent Infinite Loops with a Valid Terminal Condition
 
-// __________________________________________________ 
-
-
-
-// __________________________________________________ 
-
-
-
-// __________________________________________________ 
-
-
-
-// __________________________________________________ 
-
-
-
-// __________________________________________________ 
-
-
-
-// __________________________________________________ 
-
-
-
-// __________________________________________________ 
-
-
-
-// __________________________________________________ 
-
-
+function myFunc() {
+  for (let i = 1; i <= 4; i += 2) {
+    console.log("Still going!");
+  }
+}
 
 // __________________________________________________ 
 
