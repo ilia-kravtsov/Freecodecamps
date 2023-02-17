1566c52b72b4abdadd632580fd96a2fd46934508
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-02-17 11:37:35 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -3291,20 +3291,38 @@ function myFunc() {
   }
 }
 
-// __________________________________________________ 
+// __________________________________________________ Use an Array to Store a Collection of Data
+let yourArray = ['one', 2, 'three', true, false, undefined, null]; // Change this line
 
 
+// __________________________________________________ Access an Array's Contents Using Bracket Notation
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 'peace'
+// Only change code above this line
+console.log(myArray);
 
-// __________________________________________________ 
+// __________________________________________________ Add Items to an Array with push() and unshift()
 
+function mixedNumbers(arr) {
+  // Only change code below this line
+arr.unshift('I', 2, 'three')
+arr.push(7, 'VIII', 9)
+  // Only change code above this line
+  return arr;
+}
 
-// __________________________________________________ 
+console.log(mixedNumbers(['IV', 5, 'six']));
 
+// __________________________________________________ Remove Items from an Array with pop() and shift()
 
+function popShift(arr) {
+  let popped = arr.pop(); // Change this line
+  let shifted = arr.shift(); // Change this line
+  return [shifted, popped];
+}
 
-// __________________________________________________ 
-
-
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
 
 // __________________________________________________ 
 
