02b57cbbae87fb4d6b28b18a0ef7f5537103fa8a
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-03-02 09:56:56 +0400

    Update Free js.js

--- Free js.js
+++ Free js.js
@@ -4193,14 +4193,148 @@ singMixin : function(obj) {
 },
 }
 })() 
+// __________________________________________________ Learn About Functional Programming
+// Function that returns a string representing a cup of green tea
+const prepareTea = () => 'greenTea';
 
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+  const teaCups = [];
 
-// __________________________________________________ 
-// __________________________________________________ 
-// __________________________________________________ 
-// __________________________________________________ 
-// __________________________________________________ 
-// __________________________________________________ 
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4TeamFCC = getTea(40);
+// Only change code above this line
+// __________________________________________________ Understand Functional Programming Terminology
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+// Only change code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+// __________________________________________________ Understand the Hazards of Using Imperative Code
+// tabs is an array of titles of each site open within the window
+const Window = function(tabs) {
+  this.tabs = tabs; // We keep a record of the array inside the object
+};
+
+// When you join two windows into one window
+Window.prototype.join = function(otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
+
+// When you open a new tab at the end
+Window.prototype.tabOpen = function(tab) {
+  this.tabs.push('new tab'); // Let's open a new tab for now
+  return this;
+};
+
+// When you close a tab
+Window.prototype.tabClose = function(index) {
+
+  // Only change code below this line
+
+  const tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
+  const tabsAfterIndex = this.tabs.splice(index + 1); // Get the tabs after the tab
+
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
+
+  // Only change code above this line
+
+  return this;
+ };
+
+// Let's create three browser windows
+const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+
+// Now perform the tab opening, closing, and other operations
+const finalTabs = socialWindow
+  .tabOpen() // Open a new tab for cat memes
+  .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+  .join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
+// __________________________________________________ Avoid Mutations and Side Effects Using Functional Programming
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+
+  return fixedValue+1
+  // Only change code above this line
+}
+// __________________________________________________ Pass Arguments to Avoid External Dependence in a Function
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(value) {
+return value + 1
+
+  // Only change code above this line
+}
+// __________________________________________________ Refactor Global Variables Out of Functions
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(list, bookName) {
+  let newBookList = [...list]
+  newBookList.push(bookName);
+  return newBookList;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(list, bookName) {
+  let newBookList = [...list]
+  const book_index = newBookList.indexOf(bookName);
+  if (book_index >= 0) {
+
+    newBookList.splice(book_index, 1);
+    return newBookList;
+
+    // Change code above this line
+    }
+}
 // __________________________________________________ 
 // __________________________________________________ 
 // __________________________________________________ 
